de652f64b46a308841e072f91d5f4f210f61d81d
cedric.J <cedric_pro@mailoo.org>
2021-11-10 23:17:49 +0100

    routes_reviews_restaurant_ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  # resources :restaurants
+  resources :restaurants, only: [:index, :create, :new, :show ] do
+    resources :reviews, only: [:index, :show, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,13 @@ Restaurant.destroy_all
 
 puts "Creating restaurants..."
 dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french", phone_number: "0112325478" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+yolo = { name: "yolo", address: "56A  High St, London E1 6PQ", category: "belgian" }
+pizatato = { name: "Pizatato", address: "kiolop chelouadd", category: "italian" }
+rop = { name: "rop", address: "Bondon Ebalgueristso", category: "japanese" }
+roky = { name: "roky", address: "sjlkjskjjdsjs", category: "italian" }
 
-[dishoom, pizza_east].each do |attributes|
+
+[dishoom, yolo, pizatato, rop, roky].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
